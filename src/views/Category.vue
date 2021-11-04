187ec91a7a2072b56e7fb43b2515787f4c9024cc
amykarpiak <amyjokarpiak@gmail.com>
2021-11-04 14:36:01 -0700

    banner/images mostly done

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -1,11 +1,11 @@
 <template>
     <section id="Category">
-        <div class="hero">
+        <div class="hero" :style="{ backgroundImage: `url('${hero.image}')` }">
             <div id="headerIcon">
                 <img :src="'/img/logo.png'" id="logo">
             </div>
-            <h1>Gifts for Kids</h1>
-            <h3>Tap below to explore gift ideas to keep you moving all season</h3>
+            <h1>{{ hero.header }}</h1>
+            <h3>{{ hero.text }}</h3>
             <div id="headerIcon">
                 <img :src="'/img/frame.png'" id="frame">
             </div>
@@ -27,10 +27,12 @@
         </div>
         <div class="secondaryNav">
             <router-link to="/category/gifts-for-the-adventurer">
-                <p>< Gifts for the Adventurer</p>
+                <p>< h</p>
             </router-link>
             <router-link to="/home">
-                <img :src="'/img/homeIcon.png'" class="homeIcon">
+                <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M29.3273 19.2191C29.7585 19.5642 29.8285 20.1934 29.4834 20.6247C29.1384 21.056 28.5091 21.1259 28.0779 20.7809L26.2212 19.2955V26.962C26.2212 27.5143 25.7735 27.962 25.2212 27.962H14.7781C14.2258 27.962 13.7781 27.5143 13.7781 26.962V19.2961L11.9222 20.7809C11.4909 21.1259 10.8616 21.056 10.5166 20.6247C10.1716 20.1934 10.2415 19.5642 10.6728 19.2191L19.3753 12.2571C19.7405 11.9649 20.2595 11.9649 20.6247 12.2571L29.3273 19.2191ZM15.7781 17.6961V25.962H24.2212V17.6955L20 14.3186L15.7781 17.6961Z" fill="white"/>
+                </svg>
             </router-link>
             <router-link to="/category/gifts-for-the-fan">
                 <p>Gifts for the Fan ></p>
@@ -44,8 +46,17 @@
 export default {
     name: 'Category',
     computed: {
+        data() {
+            return {
+                categoryIndex: this.$t('categories.${ this.$route.params.id }.index')
+            }
+        },
         products() {
-            return this.$t(`categories.${ this.$route.params.id }`);
+            return this.$t(`categories.${ this.$route.params.id }.products`);
+        }, 
+
+        hero() {
+            return this.$t(`categories.${ this.$route.params.id }.hero`);
         }
     },
 }
@@ -98,7 +109,6 @@ export default {
 
     .hero{
         height: 600px;
-        background-image: url("/img/giftsforkids.png");
         background-repeat: no-repeat;
         background-size: cover;
         background-position: center;
@@ -192,6 +202,7 @@ export default {
         padding: 20px;
         display: flex;
         justify-content: space-between;
+        align-items: center;
     }
 
     .secondaryNav img{
@@ -208,10 +219,14 @@ export default {
 
     .secondaryNav a:hover{
         background: white;
-        color: red;
-        border-radius: 20px;
+        color: #E72020;
+        border-radius: 60px;
         padding: 5px;
 
+    }
+
+    svg:hover path{
+        fill: #E72020;
     }
 
 @media only screen and (max-width: 600px) {
