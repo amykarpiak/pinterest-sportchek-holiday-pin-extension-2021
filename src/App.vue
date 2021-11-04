72ad19cf65f70488fcbfd7aa871be98e22a2be20
Cody Hart <cody@thinkingbox.com>
2021-11-04 00:27:02 -0700

    [#0] Home page updates.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
     <div id="app">
         <router-view />
+        <footer>
+            <img src="/images/logo.svg" alt="SportChek">
+            <span>
+                <a href="https://sportchek.ca" target="_blank">Privacy Policy</a>
+                <span> • </span>
+                <a href="https://sportchek.ca" target="_blank">Terms of Service</a>
+            </span>
+        </footer>
     </div>
 </template>
 
@@ -14,3 +22,57 @@
     }
 
 </script>
+
+<style lang="scss" scoped>
+
+    @import './styles/_variables.scss';
+    @import './styles/_mediaqueries.scss';
+
+    footer {
+
+        width: 100%;
+        height: 60px;
+        padding: 0 50px;
+
+        background-color: color(Red);
+
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        font-family: 'Good Office Pro', sans-serif;
+        font-size: 14px;
+        color: color(White);
+
+        @include mobile-less {
+            padding: 0 25px;
+        }
+
+        @include phone {
+
+            flex-direction: column;
+            justify-content: center;
+
+            height: auto;
+            padding: 20px 25px;
+
+        }
+
+    }
+
+    footer img {
+
+        width: 80px;
+
+        @include phone {
+            margin-bottom: 20px;
+        }
+
+    }
+
+    footer a {
+        color: color(White);
+        text-decoration: none;
+    }
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,74 +1,44 @@
 <template>
     <section id="Home">
-        <img :src="'img/leftLights.png'" id="leftLight">
-        <img :src="'img/rightLights.png'" id="rightLight">
-            <div id="header">
-                <div id="headerIcon">
-                    <img :src="'img/logo.png'" id="logo">
-                </div>
-                <h2>Holiday</h2>
-                <h1>Gift Guide</h1>
-                <h3>Tap below to explore gift ideas to keep you moving all season</h3>
-                <div id="headerIcon">
-                    <img :src="'img/frame.png'" id="frame">
-                </div>
+        <div class="masthead">
+            <img class="lights left" src="/images/lights-left.png" alt="">
+            <img class="lights right" src="/images/lights-right.png" alt="">
+            <img class="logo" src="/images/logo.svg" alt="SportChek">
+            <h1>
+                <span class="small">{{ $t('home.title.line1') }}</span>
+                <span>{{ $t('home.title.line2') }}</span>
+            </h1>
+            <h2>{{ $t('home.subtitle') }}</h2>
+            <img class="logo-alt" src="/images/logo-alt.svg" alt="">
+        </div>
+        <div class="categories">
+            <div class="row one">
+                <router-link
+                    v-for="category in $t('home.categories.row1')"
+                    :key="category.title"
+                    :style="{ backgroundImage: `url(${ category.src })` }"
+                    :class="`category ${ category.shape }`"
+                    :to="`category/${ category.id }`"
+                >
+                    <h3>{{ category.title }}</h3>
+                    <img class="arrow" src="/images/red-arrow.svg" alt="">
+                </router-link>
             </div>
-            <div id="categories">
-                <div id="column">
-                    <router-link to="/category/gifts-for-her">
-                        <img :src="'img/her.png'" id="square">
-                    </router-link>
-                    <p>Gifts for Her</p>
-                </div>
-                <div id="column">
-                    <router-link to="/category/gifts-for-the-fan">
-                        <img :src="'img/fan.png'" id="circle">
-                    </router-link>
-                    <p>Gifts for the Fan</p>
-                </div>
-                <div id="column">
-                    <router-link to="/category/gifts-for-him">
-                        <img :src="'img/him.png'" id="square">
-                    </router-link>
-                    <p>Gifts for Him</p>
-                </div>
-                <div id="column">
-                    <router-link to="/category/gifts-for-the-adventurer">
-                        <img :src="'img/adventurer.png'" id="circle">
-                    </router-link>
-                    <p>Gifts for the Adventurer</p>
-                </div>
-                <div id="column">
-                    <router-link to="/category/gifts-for-kids">
-                        <img :src="'img/kids.png'" id="circle">
-                    </router-link>
-                    <p>Gifts for Kids</p>
-                </div>
-                <div id="column">
-                    <router-link to="/category/gifts-for-the-home-athlete">
-                        <img :src="'img/homeAthlete.png'" id="square">
-                    </router-link>
-                    <p>Gifts for the Home Athlete</p>
-                </div>
-                <div id="column">
-                    <router-link to="/category/gifts-for-the-hill">
-                        <img :src="'img/hill.png'" id="circle">
-                    </router-link>
-                    <p>Gifts for the Hill</p>
-                </div>
-                <div id="column">
-                    <router-link to="/category/gifts-for-warmth">
-                        <img :src="'img/warmth.png'" id="square">
-                    </router-link>
-                    <p>Gifts for Warmth</p>
-                </div>
+            <div class="row two">
+                <router-link
+                    v-for="category in $t('home.categories.row2')"
+                    :key="category.title"
+                    :style="{ backgroundImage: `url(${ category.src })` }"
+                    :class="`category ${ category.shape }`"
+                    :to="`category/${ category.id }`"
+                >
+                    <h3>{{ category.title }}</h3>
+                    <img class="arrow" src="/images/red-arrow.svg" alt="">
+                </router-link>
             </div>
-            <div id="footer">
-                <img :src="'img/logo.png'" id="logo">
-                <p>Privacy Policy•Terms & Conditions</p>
-            </div>
-
-
+        </div>
+        <img class="present left" src="/images/red-present.png" alt="">
+        <img class="present right" src="/images/grey-present.png" alt="">
     </section>
 </template>
 
@@ -81,5 +51,330 @@
 </script>
 
 <style lang="scss" scoped>
+
+    @import '../styles/_variables.scss';
+    @import '../styles/_mediaqueries.scss';
+
+    #Home {
+
+        position: relative;
+
+        color: color(White);
+        background-color: color(White);
+
+        min-height: calc(100vh - 60px);
+
+    }
+
+    #Home img.present {
+        position: absolute;
+        bottom: 0;
+        z-index: 0;
+    }
+
+    #Home img.present.left {
+
+        left: 0;
+        width: 170px;
+
+        @include phone {
+            width: 30%;
+        }
+
+    }
+
+    #Home img.present.right {
+
+        right: 0;
+        width: 240px;
+
+        @include phone {
+            width: 50%;
+        }
+
+    }
+
+    div.masthead {
+
+        position: relative;
+
+        background-image: url('/images/home-bg.png');
+        background-size: 100% 100%;
+
+        padding: 50px 0 150px;
+
+        text-align: center;
+
+        @include mobile-less {
+            background-image: url('/images/home-bg-mobile.png');
+        }
+
+    }
+
+    div.masthead img.lights {
+
+        position: absolute;
+        top: 0;
+
+        max-width: 350px;
+
+    }
+
+    div.masthead img.lights.left {
+
+        left: 0;
+        width: 20%;
+
+        @include phone {
+            width: 25%;
+        }
+
+    }
+
+    div.masthead img.lights.right {
+
+        right: 0;
+        width: 22%;
+
+        @include phone {
+            width: 30%;
+        }
+
+    }
+
+    div.masthead img.logo {
+
+        width: 170px;
+
+        @include phone {
+            width: 40%;
+        }
+
+    }
+
+    div.masthead h1,
+    div.masthead h2 {
+        font-family: 'Bagnard', serif;
+    }
+
+    div.masthead h1 {
+
+        font-size: 70px;
+        line-height: 1.1;
+
+        margin: 40px 0 30px;
+
+        @include phone {
+            font-size: 50px;
+        }
+
+    }
+
+    div.masthead h1 span {
+        display: block;
+    }
+
+    div.masthead h1 span.small {
+        font-size: 75%;
+    }
+
+    div.masthead h2 {
+
+        font-size: 25px;
+        line-height: 1.25;
+
+        margin-bottom: 30px;
+        padding: 0 25px;
+
+        @include tablet-less {
+            font-size: 20px;
+        }
+
+        @include phone {
+            font-size: 18px;
+        }
+
+    }
+
+    div.masthead img.logo-alt {
+        width: 80px;
+        transform: translateX(15%);
+    }
+
+    div.categories {
+
+        position: relative;
+        z-index: 5;
+
+        width: 100%;
+        padding: 0 50px 100px;
+        max-width: 1200px;
+        margin: -100px auto 0;
+
+        @include mobile-less {
+            display: flex;
+            flex-direction: row-reverse;
+            justify-content: space-between;
+        }
+
+        @include phone {
+            padding: 0 25px 175px;
+        }
+
+    }
+
+    div.categories div.row {
+
+        display: flex;
+        justify-content: space-between;
+
+        @include mobile-less {
+
+            justify-content: flex-start;
+            flex-direction: column;
+
+            width: calc(50% - 25px);
+
+        }
+
+        @include phone {
+            width: calc(50% - 10px);
+        }
+
+    }
+
+    div.categories div.row.two {
+
+        margin-top: 40px;
+
+
+
+    }
+
+    a.category {
+
+        position: relative;
+
+        width: calc(25% - 25px);
+        height: 0;
+        padding-top: calc(25% - 25px);
+
+        background-size: cover;
+        background-position: center;
+
+        overflow: hidden;
+
+        box-shadow: 0 10px 15px color(Black, 0.1);
+
+        color: color(White);
+        text-decoration: none;
+
+        @include mobile-less {
+            width: 100%;
+            padding-top: 100%;
+            margin-top: 25px;
+        }
+
+    }
+
+    a.category:first-of-type {
+        @include mobile-less {
+            margin-top: 0;
+        }
+    }
+
+    a.category:after {
+
+        content: '';
+
+        position: absolute;
+        bottom: 0;
+        left: 0;
+
+        width: 100%;
+        height: 100%;
+
+        background: linear-gradient(to top, color(Black, 0.4), transparent 50%);
+        border-radius: inherit;
+
+    }
+
+    a.category.square {
+        border-radius: 25px;
+    }
+
+    a.category.circle {
+        border-radius: 50%;
+    }
+
+    a.category h3 {
+
+        position: absolute;
+        left: 50%;
+        z-index: 5;
+
+        transform: translateX(-50%);
+
+        font-family: 'Good Office Pro Bold', sans-serif;
+        font-size: 28px;
+        line-height: 1.1;
+        text-align: center;
+        white-space: pre;
+
+        @include desktop {
+            font-size: 24px;
+        }
+
+        @include tablet {
+            font-size: 22px;
+        }
+
+        @include tablet-less {
+            font-size: 18px;
+        }
+
+    }
+
+    a.category.square h3 {
+        bottom: 8%;
+    }
+
+    a.category.circle h3 {
+        bottom: 12%;
+    }
+
+    a.category img.arrow {
+
+        position: absolute;
+        right: 0;
+        z-index: 5;
+
+        width: 40px;
+
+        @include tablet-less {
+            width: 30px;
+        }
+
+    }
+
+    a.category.square img.arrow {
+
+        bottom: 25px;
+
+        @include tablet-less {
+
+            bottom: auto;
+            top: 50%;
+
+            transform: translateY(-50%);
+
+        }
+
+    }
+
+    a.category.circle img.arrow {
+        top: 50%;
+        transform: translateY(-50%);
+    }
 
 </style>
