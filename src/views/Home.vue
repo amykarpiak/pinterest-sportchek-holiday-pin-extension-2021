462f28b950db0bfecb32edd707a03ec069aecc7a
amykarpiak <amyjokarpiak@gmail.com>
2021-11-08 13:41:42 -0800

    [#1] preload complete

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -58,6 +58,98 @@
         components: {
             Scroll,
         },
+        created() {
+            const loader = preloader({ xhrImages: false });
+            loader.on('complete', this.onLoadComplete);
+
+            //home
+            loader.addImage('/images/categories/her/bg.jpg');
+            loader.addImage('/images/categories/fan/bg.jpg');
+            loader.addImage('/images/categories/him/bg.jpg');
+            loader.addImage('/images/categories/adventurer/bg.jpg');
+            loader.addImage('/images/categories/kids/bg.jpg');
+            loader.addImage('/images/categories/athlete/bg.jpg');
+            loader.addImage('/images/categories/hill/bg.jpg');
+            loader.addImage('/images/categories/warmth/bg.jpg');
+
+            //her
+            loader.addImage('/images/products/her/hero.jpg');
+            loader.addImage('/images/products/her/responsive.jpg');
+            loader.addImage('/images/products/her/NikeWomensAirMax90_365Shoes.png');
+            loader.addImage('/images/products/her/HellyHansenWomensLegendaryInsulatedSnowPants.png');
+            loader.addImage('/images/products/her/Nike_Womens_Yoga_Layer_Tank.png');
+            loader.addImage('/images/products/her/ColumbiaWomen_sPlusSizeLay_D_DownOmni-HEATJacket1.png');
+            loader.addImage('/images/products/her/WoodsWomen_sBenningtonDownMitts1.png');
+
+            //fan
+            loader.addImage('/images/products/fan/hero.jpg');
+            loader.addImage('/images/products/fan/responsive.jpg');
+            loader.addImage('/images/products/fan/NikeTeamCanadaReplicaHockeyJersey.png');
+            loader.addImage('/images/products/fan/TeamCanadaNikeInfantHockeyJersey.png');
+            loader.addImage('/images/products/fan/TorontoRaptorsNikeMensPascalSiakamIconJersey.png');
+            loader.addImage('/images/products/fan/TeamCanadaNikeSlouchHeritage86Cap.png');
+            loader.addImage('/images/products/fan/TorontoRaptorsNewEra9FIFTYDraftCap.png');
+
+            //him
+            loader.addImage('/images/products/him/hero.jpg');
+            loader.addImage('/images/products/him/responsive.jpg');
+            loader.addImage('/images/products/him/HurleyDunesHeatPulloverHoody1.png');
+            loader.addImage('/images/products/him/adidasgumshoes.png');
+            loader.addImage('/images/products/him/NikeMensRestoreDri-FITFleecePants.png');
+            loader.addImage('/images/products/him/HellyHansenMensBarentsInsulatedBomberJacket.png');
+            loader.addImage('/images/products/him/WoodsMensThomlinsonFlannelShacket.png');
+
+            //adventurer
+            loader.addImage('/images/products/adventurer/hero.jpg');
+            loader.addImage('/images/products/adventurer/responsive.jpg');
+            loader.addImage('/images/products/adventurer/FitbitCharge5.png');
+            loader.addImage('/images/products/adventurer/MerrellMensErieMidWaterproofHikingShoes.png');
+            loader.addImage('/images/products/adventurer/OspreyKatari7LHydrationBackpack.png');
+            loader.addImage('/images/products/adventurer/GTAvalanche29MountainBike2021.png');
+            loader.addImage('/images/products/adventurer/JetBoilFlashStove2.0-Carbon.png');
+
+            //kids 
+            loader.addImage('/images/products/kids/hero.jpg');
+            loader.addImage('/images/products/kids/responsive.jpg');
+            loader.addImage('/images/products/kids/CCMSuperTacks9350YouthHockeySkates.png');
+            loader.addImage('/images/products/kids/UABoysProntoCBINSPufferJacket.png');
+            loader.addImage('/images/products/kids/NikeGirlsGradeSchoolRevolution5RunningShoes.png');
+            loader.addImage('/images/products/kids/TheNorthFaceInfantCampshireOnePieceSuit.png');
+            loader.addImage('/images/products/kids/adidasOriginalsKidsGradeSchoolForumMidShoes.png');
+
+            //home-athlete
+            loader.addImage('/images/products/athlete/hero.jpg');
+            loader.addImage('/images/products/athlete/responsive.jpg');
+            loader.addImage('/images/products/athlete/UAWomensMeridianTightMauvePink.png');
+            loader.addImage('/images/products/athlete/PTPTotalResistanceSystem.png');
+            loader.addImage('/images/products/athlete/NikeWomensAirZoonSuperRep2TrainingShoes.png');
+            loader.addImage('/images/products/athlete/BowflexSeletTech552AdjustableDumbbell.png');
+            loader.addImage('/images/products/athlete/UAMensTrainingVent2.0GraphicLongSleeveShirt.png');
+
+            //the-hill
+            loader.addImage('/images/products/hill/hero.jpg');
+            loader.addImage('/images/products/hill/responsive.jpg');
+            loader.addImage('/images/products/hill/22.png');
+            loader.addImage('/images/products/hill/burton.png');
+            loader.addImage('/images/products/hill/VolklRacetigerJuniorSkis2020_21 _Marker41.png');
+            loader.addImage('/images/products/hill/OakleyFlightDeck™MWomensSki&SnowboardGoggles.png');
+            loader.addImage('/images/products/hill/volklskis.png');
+            loader.addImage('/images/products/hill/AtomicHawkMagna75WomensSkiBoots1.png');
+            loader.addImage('/images/products/hill/GiroAveraMIPSWomensSki&SnowboardHelmet.png');
+
+            //warmth
+            loader.addImage('/images/products/warmth/hero.jpg');
+            loader.addImage('/images/products/warmth/responsive.jpg');
+            loader.addImage('/images/products/warmth/PurSerenity8lbWeightedOutdoorBlanket.png');
+            loader.addImage('/images/products/warmth/NikeSportswearMensClubBBPants.png');
+            loader.addImage('/images/products/warmth/SaxxMensDownTimeFullZipHoodie.png');
+            loader.addImage('/images/products/warmth/WoodsWomensErrisSherpaFleeceJacket.png');
+            loader.addImage('/images/products/warmth/NikeWomensColdWeatherKnitGloves.png');
+
+            // Add additional assets here.
+
+            loader.load();
+        }
     }
 
 </script>
